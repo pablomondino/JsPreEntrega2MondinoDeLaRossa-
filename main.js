820f0bda5f5a05e471b91f861edd10de451a252d
Pablo Mondino De La Rossa <p_mondino@hotmail.com>
2023-08-21 19:14:07 -0300

    Ordenando menú

--- main.js
+++ main.js
@@ -31,39 +31,77 @@ if (ingresar) {
   let entregaEnEfectivo = 0;
   let tipologiaElegida = "";
   let eleccion = [];
+
   const tipoDeLotesDisponibles = [
     { tipologia: "a", tamano: 250, frente: 10, fondo: 25, ubicacion: "Esquina de manzana" },
     { tipologia: "b", tamano: 360, frente: 12, fondo: 30, ubicacion: "Dentro de manzana" },
     { tipologia: "c", tamano: 450, frente: 15, fondo: 30, ubicacion: "Dentro de manzana" },
     { tipologia: "d", tamano: 560, frente: 16, fondo: 35, ubicacion: "Dentro de manzana" }];
 
-  let elegirOpcion = prompt("Elegí tu opcion\n 1- Valor total del terreno elegido\n 2-De cuanto es la entrega en efectivo que desea realizar? \n 3-¿Cual es el saldo para financiar?\n 4-¿En cuantas cuotas desea financiar el saldo?\n 5-Cómo quedaría el plan de financiación elegido\n 6-mostrar todos los lotes disponibles\n 7-Elegir tipología(a-b-c-d)\nPresione x para salir ");
+  let elegirOpcion = prompt("Elegí tu opcion\n 1-Mostrar todos los tipos de lotes disponibles\n 2-Elegir tipología(a-b-c-d)\n 3-Valor total del terreno elegido\n 4-De cuanto es la entrega en efectivo que desea realizar? \n 5-¿Cual es el saldo para financiar?\n 6-¿En cuantas cuotas desea financiar el saldo?\n 7-Cómo quedaría el plan de financiación elegido\n Presione x para salir ");
   while (elegirOpcion != "x") {
     switch (elegirOpcion) {
 
-
-
       case "1":
-        //alert("Elegiste la opción 1\n  El valor total del terreno es de " + valorTotalTerreno + " pesos");
-        if (valorTotalTerreno != 0) {
-         // alert("el valor total es igual a " + eleccion[0].tamano);
-          alert("Elegiste la opción 1\n  El valor total del terreno es de " + valorTotalTerreno + " pesos");
+        alert(" Elegiste 1- Mostrar todos los lotes disponibles por tipología ");
+        //alert( "lote 1- lote 2 - lote 3");
+        tipoDeLotesDisponibles.forEach(element => {
+          alert("Tipología: " + element.tipologia + "\nTamaño de lote: " + element.tamano + "\n Ubicación: " + element.ubicacion + "\n Medidas\n frente: " + element.frente + "\n fondo: " + element.fondo);
 
-          // el metro cuadrado sale 1000 pesos
-          console.log(eleccion[0].tamano)
-        } else {
-          alert("debe elegir una tipología")
+          //console.log(element);
 
+        });
+        //alert(tipoDeLotesDisponibles);
+        break;
+
+      case "2":
+        function filtrarPorTipologia(arr, filtro) {
+          return arr.filter(elem => elem.tipologia == filtro);
 
         }
+        tipologiaElegida = prompt("Elejiste opción 2.\n Elija una tipología (a,b,c,d)");
+        
+        console.log(tipologiaElegida);
+        eleccion = filtrarPorTipologia(tipoDeLotesDisponibles, tipologiaElegida);
+        console.log();
 
+
+
+
+        alert(" Ud eligió la tipología: " + eleccion[0].tipologia);
+        console.log(eleccion[0]);
+        
+        valorTotalTerreno = eleccion[0].tamano * 1000;
+        //nota el valor del metro cuadrado es de 1000 pesos
+        console.log(valorTotalTerreno)
 
 
         break;
 
-      case "2":
-        entregaEnEfectivo = parseFloat(prompt("Elegiste la opcion 2\n ¿Cuanto es la entrega en efectivo que desea realizar?"));
 
+
+
+
+
+      case "3":
+        
+        if (valorTotalTerreno != 0) {
+          alert("Elegiste la opción 3\n  El valor total del terreno es de " + valorTotalTerreno + " pesos");
+
+       
+          console.log(eleccion[0].tamano)
+        } else {
+          alert("Debe elegir una tipología")
+
+
+        }
+
+        break;
+
+
+      case "4":
+        entregaEnEfectivo = parseFloat(prompt("Elegiste la opcion 4\n ¿Cuanto es la entrega en efectivo que desea realizar?"));
+4
         if (entregaEnEfectivo >= 200) {
           saldo = valorTotalTerreno - entregaEnEfectivo;
           alert("Usted desea entregar " + entregaEnEfectivo + " pesos\n Tu entrega está correcta. Supera el monto minimo. ");
@@ -76,98 +114,28 @@ if (ingresar) {
 
         break;
 
-      case "3":
-        alert("Elegiste la opción 3\n  El saldo que desea financiar es de " + saldo + " pesos");
-
-        break;
-
-      case "4":
-        cantidadDeCuotas = parseFloat(prompt("elija una opcion: \n puede elegir de 2 a 30 cuotas"));
-        if (cantidadDeCuotas <= 30 && cantidadDeCuotas >= 2) {
-          alert("elegiste " + cantidadDeCuotas + "cuotas")
-        } else {
-          alert(" las cuotas deben ser entre  2 y 30 cuotas");
-        }
-
-
-        break;
-
-
-
-
       case "5":
-        alert("Elegiste la opción 5 \n Valor total del lote es de " + valorTotalTerreno + " pesos. La entrega inicial es de " + entregaEnEfectivo + " y el saldo de " + saldo + " pesos a pagar en " + cantidadDeCuotas + " de cuotas de " + (saldo / cantidadDeCuotas) + " pesos");
+        alert("Elegiste la opción 5\n  El saldo que desea financiar es de " + saldo + " pesos");
+
         break;
 
       case "6":
-        alert(" Elegiste 6 -Mostrar todos los lotes disponibles por tipología ");
-        //alert( "lote 1- lote 2 - lote 3");
-        tipoDeLotesDisponibles.forEach(element => {
-          alert("Tipología: " + element.tipologia + "\nTamaño de lote: " + element.tamano + "\n Ubicación: " + element.ubicacion + "\n Medidas\n frente: " + element.frente + "\n fondo: " + element.fondo);
-
-          //console.log(element);
-
-        });
-        //alert(tipoDeLotesDisponibles);
-        break;
-
-      case "7":
-        function filtrarPorTipologia(arr, filtro) {
-          return arr.filter(elem => elem.tipologia == filtro);
-
-        }
-        tipologiaElegida = prompt("Elija una tipología");
-        /*
-                function filtrarPorTipologia(arr,filtro){
-                  return arr.filter(elem=>elem.tipologia==filtro);
-                  
-                }
-          */
-        console.log(tipologiaElegida);
-        eleccion = filtrarPorTipologia(tipoDeLotesDisponibles, tipologiaElegida);
-        console.log();
-
-
-
-
-        alert(" ud eligio la tipología: " + eleccion[0].tipologia);
-        console.log(eleccion[0]);
-        //cambie esto
-        //valorTotalTerreno=parseFloat((eleccion[0])*1000);
-        
-        valorTotalTerreno=eleccion[0].tamano*1000;
-        //nota el valor del metro cuadrado es de 1000 pesos
-        console.log(  valorTotalTerreno)
-
-        //---------------------------------------------------
-        /*
-        const carritofiltrado=carrito.filter((ele)=>{
-          return ele.precio<=600
-        })
-        console.log(carritofiltrado);
-        */
-        //------------------------------------------------------
-
-
-
-
-
-
-        //elegir tipología
-        /*
-        cantidadDeCuotas = parseFloat(prompt("elija una opcion: \n puede elegir de 2 a 30 cuotas"));
+        cantidadDeCuotas = parseFloat(prompt("Elija una opcion : \n puede elegir de 2 a 30 cuotas"));
         if (cantidadDeCuotas <= 30 && cantidadDeCuotas >= 2) {
           alert("elegiste " + cantidadDeCuotas + "cuotas")
         } else {
           alert(" las cuotas deben ser entre  2 y 30 cuotas");
         }
-        */
+
 
         break;
 
 
 
 
+      case "7":
+        alert("Elegiste la opción 7 \n Valor total del lote es de " + valorTotalTerreno + " pesos. La entrega inicial es de " + entregaEnEfectivo + " y el saldo de " + saldo + " pesos a pagar en " + cantidadDeCuotas + " de cuotas de " + (saldo / cantidadDeCuotas) + " pesos");
+        break;
 
 
 
@@ -182,7 +150,7 @@ if (ingresar) {
 
 
     }
-    elegirOpcion = prompt("Elegí tu opcion\n 1- Valor total del terreno elegido\n 2-De cuanto es la entrega en efectivo que desea realizar? \n 3-¿Cual es el saldo para financiar?\n 4-¿En cuantas cuotas desea financiar el saldo?\n 5-Cómo quedaría el plan de financiación elegido\n 6-mostrar todos los lotes disponibles\n 7-Elegir tipología(a-b-c-d)\nPresione x para salir ");
+    elegirOpcion = prompt("Elegí tu opcion\n 1-Mostrar todos los tipos de lotes disponibles\n 2-Elegir tipología(a-b-c-d)\n 3-Valor total del terreno elegido\n 4-De cuanto es la entrega en efectivo que desea realizar? \n 5-¿Cual es el saldo para financiar?\n 6-¿En cuantas cuotas desea financiar el saldo?\n 7-Cómo quedaría el plan de financiación elegido \n Presione x para salir ");
   }
   alert("gracias por operar con nosotros");
 
